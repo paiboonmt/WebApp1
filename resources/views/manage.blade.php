b425e619938d6d5d552873b7594ccc315a01843a
paiboonmt <paiboon500@gmail.com>
2024-09-21 14:07:11 +0700

    up

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -25,16 +25,39 @@
                                         <tr>
                                             <th>#</th>
                                             <th>Name</th>
+                                            <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         @php
-                                            $i=1;
+                                            $i = 1;
                                         @endphp
                                         @foreach ($dataFighter as $frow)
                                             <tr>
                                                 <td>{{ $i++ }}</td>
                                                 <td>{{ $frow->fname }}</td>
+                                                <td>
+                                                    <button type="button" class="btn btn-sm btn-primary"
+                                                        data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                                        <i class="fa-solid fa-binoculars"></i> | View
+                                                    </button>
+                                                </td>
+
+                                                <!-- Modal -->
+                                                <div class="modal fade" id="exampleModal" tabindex="-1"
+                                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                ...
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
                                             </tr>
                                         @endforeach
                                     </tbody>
